ac9dc01579472da4b6b8f2b8a423f76c6b776912
ohsine <ohsine@korea.ac.kr>
2022-11-27 18:18:55 +0900

    1127_정리

--- travel-website/src/components/Footer.js
+++ travel-website/src/components/Footer.js
@@ -29,44 +29,44 @@ function Footer() {
         </section>
 
         <div className="footer-links">
-        <div className='footer-link-wrapper'>
-            <div class='footer-link-items'>
-                <h2>About Us</h2>
+            <div className='footer-link-wrapper'>
+                <div class='footer-link-items'>
+                    <h2>About Us</h2>
 
-                <Link to='/sign-up'>How it works</Link>
-                <Link to='/'>Testimonials</Link>
-                <Link to='/'>Careers</Link>
-                <Link to='/'>Investors</Link>
-                <Link to='/'>Terms of Service</Link>
+                    <Link to='/sign-up'>How it works</Link>
+                    <Link to='/'>Testimonials</Link>
+                    <Link to='/'>Careers</Link>
+                    <Link to='/'>Investors</Link>
+                    <Link to='/'>Terms of Service</Link>
+                </div>
+
+                <div class='footer-link-items'>
+                    <h2>Contact Us</h2>
+
+                    <Link to='/'>Contact</Link>
+                    <Link to='/'>Support</Link>
+                    <Link to='/'>Destinations</Link>
+                    <Link to='/'>Sponsorships</Link>
+                </div>
             </div>
 
-            <div class='footer-link-items'>
-                <h2>Contact Us</h2>
+            <div className='footer-link-wrapper'>
+                <div class='footer-link-items'>
+                    <h2>Videos</h2>
 
-                <Link to='/'>Contact</Link>
-                <Link to='/'>Support</Link>
-                <Link to='/'>Destinations</Link>
-                <Link to='/'>Sponsorships</Link>
-            </div>
-        </div>
+                    <Link to='/'>Submit Video</Link>
+                    <Link to='/'>Ambassadors</Link>
+                    <Link to='/'>Agency</Link>
+                    <Link to='/'>Influencer</Link>
+                </div>
+                
+                <div class='footer-link-items'>
+                    <h2>Social Media</h2>
 
-        <div className='footer-link-wrapper'>
-            <div class='footer-link-items'>
-                <h2>Videos</h2>
-
-                <Link to='/'>Submit Video</Link>
-                <Link to='/'>Ambassadors</Link>
-                <Link to='/'>Agency</Link>
-                <Link to='/'>Influencer</Link>
-            </div>
-            
-            <div class='footer-link-items'>
-                <h2>Social Media</h2>
-
-                <Link to='/'>Instagram</Link>
-                <Link to='/'>Facebook</Link>
-                <Link to='/'>Youtube</Link>
-                <Link to='/'>Twitter</Link>
+                    <Link to='/'>Instagram</Link>
+                    <Link to='/'>Facebook</Link>
+                    <Link to='/'>Youtube</Link>
+                    <Link to='/'>Twitter</Link>
                 </div>
             </div>
         </div>
